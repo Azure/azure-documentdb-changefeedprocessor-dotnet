f676291bff1309b3cb77371dacdf2b1824651602
Matias Quaranta <ealsur@hotmail.com>
2018-03-28 08:31:17 -0300

    Single-lining comment

--- src/DocumentDB.ChangeFeedProcessor/PartitionManagement/IRemainingWorkEstimator.cs
+++ src/DocumentDB.ChangeFeedProcessor/PartitionManagement/IRemainingWorkEstimator.cs
@@ -7,13 +7,12 @@ namespace Microsoft.Azure.Documents.ChangeFeedProcessor.PartitionManagement
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Estimates pending work to be read in the Change Feed.
-    /// Calculates the sum of pending work based on the difference between the latest status of the feed and the status of each existing lease.
+    /// Used to estimate the pending work remaining to be read in the Change Feed. Calculates the sum of pending work based on the difference between the latest status of the feed and the status of each existing lease.
     /// </summary>
     public interface IRemainingWorkEstimator
     {
         /// <summary>
-        /// Calculates an estimate of the pending work remaining to read in the Change Feed in amount of documents.
+        /// Calculates an estimate of the pending work remaining to be read in the Change Feed in amount of documents.
         /// </summary>
         /// <returns>An estimation of pending work in amount of documents.</returns>
         Task<long> GetEstimatedRemainingWork();
