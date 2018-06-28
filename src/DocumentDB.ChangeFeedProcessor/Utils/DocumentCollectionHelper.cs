fe9591e49d9478e61429b4a96edf26ac18c09fac
mkolt <mkolt@microsoft.com>
2018-06-28 13:14:41 -0700

    (1) Move back to .net45 to be consistent with SDK, (2) remove prerele… (#88)
    * (1) Move back to .net45 to be consistent with SDK, (2) remove prerelease (3) increase version to 2.0.4.
    
    The idea with 451 is that SDK is targeting .net45 so we should be consistent and one internal customer is sort of blocked because they use .net45. But main idea is to be consistent with SDK. When SDK moves up, CFP will do the same.

--- src/DocumentDB.ChangeFeedProcessor/Utils/DocumentCollectionHelper.cs
+++ src/DocumentDB.ChangeFeedProcessor/Utils/DocumentCollectionHelper.cs
@@ -9,7 +9,7 @@ namespace Microsoft.Azure.Documents.ChangeFeedProcessor.Utils
 
     internal static class DocumentCollectionHelper
     {
-        private const string DefaultUserAgentSuffix = "changefeed-2.0.3";
+        private const string DefaultUserAgentSuffix = "changefeed-2.0.4";
 
         public static DocumentCollectionInfo Canonicalize(this DocumentCollectionInfo collectionInfo)
         {
