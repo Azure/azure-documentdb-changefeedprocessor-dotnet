bee0d35e7796afdf335f4f3e18d3d57a17ce3b24
Michael Koltachev <mkolt@microsoft.com>
2018-10-30 12:52:19 -0700

    Support for PK lease collections: fix Lease Manager: pass request options with PK for delete. This is only used in split scenario.
    This has 2 changes:
    1) cherry-pick https://github.com/Azure/azure-documentdb-changefeedprocessor-dotnet/pull/115/commits/388a5879106d02a2438e2fb9c09dd7633c10460c and merge/resolve
    2) Bump the version 2.2.1 -> 2.2.2.

--- src/DocumentDB.ChangeFeedProcessor/Utils/DocumentCollectionHelper.cs
+++ src/DocumentDB.ChangeFeedProcessor/Utils/DocumentCollectionHelper.cs
@@ -10,7 +10,7 @@ namespace Microsoft.Azure.Documents.ChangeFeedProcessor.Utils
 
     internal static class DocumentCollectionHelper
     {
-        private const string DefaultUserAgentSuffix = "changefeed-2.2.1";
+        private const string DefaultUserAgentSuffix = "changefeed-2.2.2";
 
         public static DocumentCollectionInfo Canonicalize(this DocumentCollectionInfo collectionInfo)
         {
