0ded21cda2573d281b1f2bd1f00ecba8fcacda93
Matias Quaranta <ealsur@hotmail.com>
2018-12-11 10:15:26 -0800

    Adding tests

--- src/DocumentDB.ChangeFeedProcessor.UnitTests/PartitionManagement/PartitionSynchronizerTests.cs
+++ src/DocumentDB.ChangeFeedProcessor.UnitTests/PartitionManagement/PartitionSynchronizerTests.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation.  Licensed under the MIT license.
 //----------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
@@ -82,6 +83,92 @@ namespace Microsoft.Azure.Documents.ChangeFeedProcessor.UnitTests.PartitionManag
             var leaseContainer = Mock.Of<ILeaseContainer>();
 
             var sut = new PartitionSynchronizer(documentClient, "collectionlink", leaseContainer, leaseManager, 1, int.MaxValue);
+            IEnumerable<ILease> result = await sut.SplitPartitionAsync(lease);
+            Assert.NotNull(result);
+            Assert.Equal(new[] { lease20 }, result);
+        }
+
+        [Fact]
+        public async Task SplitPartitionAsync_ShouldThrow_IfSinglePartitionAndChildPartitionsLessThan2()
+        {
+            const string lastKnowToken = "last know token";
+            const string collectionLink = "collectionLink";
+
+            IEnumerable<PartitionKeyRange> keyRanges = new[]
+            {
+                new PartitionKeyRange
+                {
+                    Id = "20", Parents = new Collection<string>(new[] {"10"})
+                }
+            };
+
+            IEnumerable<Offer> offers = new[]
+            {
+                new Offer
+                {
+                    Id = "1",
+                    ResourceId = collectionLink
+                }
+            };
+
+            var lease = Mock.Of<ILease>(l => l.PartitionId == "10" && l.ContinuationToken == lastKnowToken);
+
+            var keyRangeResponse = Mock.Of<IFeedResponse<PartitionKeyRange>>(r => r.GetEnumerator() == keyRanges.GetEnumerator());
+            var offerResponse = Mock.Of<IFeedResponse<Offer>>(r => r.GetEnumerator() == offers.GetEnumerator());
+            IChangeFeedDocumentClient documentClient = Mock.Of<IChangeFeedDocumentClient>(c =>
+                c.ReadPartitionKeyRangeFeedAsync(It.IsAny<string>(), It.IsAny<FeedOptions>()) == Task.FromResult(keyRangeResponse)
+                && c.ReadOffersFeedAsync(It.IsAny<FeedOptions>()) == Task.FromResult(offerResponse)
+                );
+
+            var lease20 = Mock.Of<ILease>();
+            ILeaseManager leaseManager = Mock.Of<ILeaseManager>(m =>
+                m.CreateLeaseIfNotExistAsync("20", lastKnowToken) == Task.FromResult(lease20));
+            var leaseContainer = Mock.Of<ILeaseContainer>();
+
+            var sut = new PartitionSynchronizer(documentClient, collectionLink, leaseContainer, leaseManager, 1, int.MaxValue);
+            Exception exception = await Record.ExceptionAsync(async () => await sut.SplitPartitionAsync(lease));
+            Assert.IsAssignableFrom<InvalidOperationException>(exception);
+        }
+
+        [Fact]
+        public async Task SplitPartitionAsync_ShouldPass_IfPartitionedAndChildPartitionsLessThan2()
+        {
+            const string lastKnowToken = "last know token";
+            const string databaseLink = "databaseLink";
+            const string collectionLink = "collectionLink";
+
+            IEnumerable<PartitionKeyRange> keyRanges = new[]
+            {
+                new PartitionKeyRange
+                {
+                    Id = "20", Parents = new Collection<string>(new[] {"10"})
+                }
+            };
+
+            IEnumerable<Offer> offers = new[]
+            {
+                new Offer
+                {
+                    Id = "1", 
+                    ResourceId = databaseLink
+                }
+            };
+
+            var lease = Mock.Of<ILease>(l => l.PartitionId == "10" && l.ContinuationToken == lastKnowToken);
+
+            var keyRangeResponse = Mock.Of<IFeedResponse<PartitionKeyRange>>(r => r.GetEnumerator() == keyRanges.GetEnumerator());
+            var offerResponse = Mock.Of<IFeedResponse<Offer>>(r => r.GetEnumerator() == offers.GetEnumerator());
+            IChangeFeedDocumentClient documentClient = Mock.Of<IChangeFeedDocumentClient>(c =>
+                c.ReadPartitionKeyRangeFeedAsync(It.IsAny<string>(), It.IsAny<FeedOptions>()) == Task.FromResult(keyRangeResponse)
+                && c.ReadOffersFeedAsync(It.IsAny<FeedOptions>()) == Task.FromResult(offerResponse)
+                );
+
+            var lease20 = Mock.Of<ILease>();
+            ILeaseManager leaseManager = Mock.Of<ILeaseManager>(m =>
+                m.CreateLeaseIfNotExistAsync("20", lastKnowToken) == Task.FromResult(lease20));
+            var leaseContainer = Mock.Of<ILeaseContainer>();
+
+            var sut = new PartitionSynchronizer(documentClient, collectionLink, leaseContainer, leaseManager, 1, int.MaxValue);
             IEnumerable<ILease> result = await sut.SplitPartitionAsync(lease);
             Assert.NotNull(result);
             Assert.Equal(new[] { lease20 }, result);
