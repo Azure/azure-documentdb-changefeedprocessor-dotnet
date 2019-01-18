d10fd522b48fc8d820ff72d07992c5479bdf17a6
Matias Quaranta <ealsur@hotmail.com>
2019-01-18 11:30:12 -0800

    Adding UserException

---
+++ src/DocumentDB.ChangeFeedProcessor/Exceptions/UserException.cs
@@ -0,0 +1,26 @@
+﻿//----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  Licensed under the MIT license.
+//----------------------------------------------------------------
+
+namespace Microsoft.Azure.Documents.ChangeFeedProcessor.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Exception occurred when ProcessChangesAsync is running and thrown by user code
+    /// </summary>
+    [Serializable]
+    public class UserException : Exception
+    {
+        private static readonly string DefaultMessage = "Exception has been thrown by ProcessChangesAsync.";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserException" /> class using the specified internal exception.
+        /// </summary>
+        /// <param name="internalException"><see cref="Exception"/> thrown by the user code.</param>
+        public UserException(Exception internalException)
+            : base(UserException.DefaultMessage, internalException)
+        {
+        }
+    }
+}
